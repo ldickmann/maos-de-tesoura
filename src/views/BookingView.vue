85ed5cf9b539cdc921ccfa4597f61822988aa548
ldickmann <ldickmann12@gmail.com>
2025-09-07 01:42:43 -0300

    FEAT: Remove duplicação de importações de estilos no componente BookingView, melhorando a organização do código

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -378,6 +378,8 @@ window.addEventListener(
 <style scoped lang="scss">
 @use 'sass:color';
 @use '@/assets/styles/scss/_variables.scss' as var;
+@use 'sass:color';
+@use '@/assets/styles/scss/_variables.scss' as var;
 .booking-page {
   padding: 88px 2rem 2rem;
   background-color: var.$bg-light;
