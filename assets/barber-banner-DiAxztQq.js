2c1ca0b3f75e9d985ca57583a3185fd9ca96192d
ldickmann <ldickmann12@gmail.com>
2025-09-07 05:02:28 -0300

    Updates

--- assets/barber-banner-DiAxztQq.js
+++
@@ -1,1 +0,0 @@
-const a="/maos-de-tesoura/assets/image-1-card-gZaUvuYV.jpg",s="/maos-de-tesoura/assets/barber-banner-DsR8esAx.jpg";export{s as b,a as c};
---
+++ assets/image-1-card-CyIW3nYn.js
@@ -0,0 +1,1 @@
+const a="/maos-de-tesoura/assets/image-1-card-gZaUvuYV.jpg";export{a as c};
